3ce7e28ff023c5731ca498bce019db58eb5d813c
myFirstgitrepo <tanvirmahin607@gmail.com>
2025-01-22 01:16:02 +0600

    added session post and session get

--- index.js
+++ index.js
@@ -1,18 +1,70 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 // middle ware
 app.use(cors())
 app.use(express.json())
 
+// DB_PASS=4GDmDDD9qcPChmXh
+// DB_USER=LearnHub
 
-app.get('/', (req, res)=>{
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kriop.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+        //*-----------|| All Api collection ||----------
+        const sessionCollection = client.db("LearnHub").collection('sessions');
+
+        //* ------------|| Api creation ||----------
+        
+        //! || Session post method ||
+        app.post('/session', async(req, res)=>{
+            const session = req.body;
+            const result = await sessionCollection.insertOne(session);
+            res.send(result);
+        })
+ 
+        //! || Session get method ||
+        app.get('/session', async(req, res)=>{
+            const result = await sessionCollection.find().toArray();
+            res.send(result)
+        }) 
+
+
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
+app.get('/', (req, res) => {
     res.send('Go to Study please!!!!')
 })
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`server is running at port: ${port}`);
 })
 
