845c224030f1d5023d409a4daae7d75dc1c065f2
myFirstgitrepo <tanvirmahin607@gmail.com>
2025-01-23 22:30:09 +0600

    impliment a jwt token

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const jwt = require('jsonwebtoken');
 const express = require('express');
 const cors = require('cors');
 const app = express();
@@ -8,9 +9,6 @@ const port = process.env.PORT || 5000;
 // middle ware
 app.use(cors())
 app.use(express.json())
-
-// DB_PASS=4GDmDDD9qcPChmXh
-// DB_USER=LearnHub
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kriop.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -32,60 +30,92 @@ async function run() {
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
+
         //*-----------|| All Api collection ||----------
         const sessionCollection = client.db("LearnHub").collection('sessions');
         const materialCollection = client.db("LearnHub").collection('materials');
+        const userCollection = client.db("LearnHub").collection('users');
 
-        //* ------------|| Api creation ||----------
+
+        //*-----------|| JWT Api ||----------
+        app.post('/jwt', async (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.JWT_SECRET, { expiresIn: '5h' });
+            res.send({ token });
+        })
+
+        //*-----------|| Middleware ||----------
+        const verifyToken = (req, res, next) => {
+            // console.log('from verifyToken', req.headers);
+            if (!req.headers?.authorization) {
+                return res.status(401).send({ message: "unauthorize access" })
+            }
+            const token = req.headers.authorization.split(' ')[1];
+            jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+                if (err) {
+                    res.status(401).send({ message: "unauthorized access" });
+                }
+                req.decoded = decoded;
+                next();
+            })
+        }
+
+        //* ------------|| User Api ||----------
+        app.post('/user', async(req, res)=>{
+            const userInfo = req.body;
+            const result = await userCollection.insertOne(userInfo);
+            res.send(result)
+        })
 
         //* ------------|| Session Api ||----------
-        
+
         //! || Session post method ||
-        app.post('/session', async(req, res)=>{
+        app.post('/session', async (req, res) => {
             const session = req.body;
             const result = await sessionCollection.insertOne(session);
             res.send(result);
         })
- 
+
         //! || Session get method ||
-        app.get('/session', async(req, res)=>{
+        app.get('/session', async (req, res) => {
             const result = await sessionCollection.find().toArray();
             res.send(result)
-        }) 
+        })
 
         //! || Session get by id method ||
-        app.get('/session/:id', async(req, res)=>{
+        app.get('/session/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id: new ObjectId(id)};
+            const query = { _id: new ObjectId(id) };
             const result = await sessionCollection.findOne(query);
             res.send(result)
         })
 
         //! || Session get by email method ||
-        app.get('/personalSession/:email', async(req, res)=>{
+        app.get('/personalSession/:email', verifyToken, async (req, res) => {
+            // console.log(req.headers);
             const email = req.params.email;
-            const query = {tutorEmail: email};
+            const query = { tutorEmail: email };
             const result = await sessionCollection.find(query).toArray();
             res.send(result)
         })
-        
+
         //* ------------|| Material Api ||----------
 
-         //! || material post method ||
-         app.post('/materials', async(req, res)=>{
+        //! || material post method ||
+        app.post('/materials', async (req, res) => {
             const material = req.body;
             const email = req.query.email;
             const id = req.query.id;
-            const query = {email: email, sessionId: id};
+            const query = { email: email, sessionId: id };
             const existing = await materialCollection.findOne(query);
-            if(existing){
-                return res.status(400).send({message: "Card Already exist"})
+            if (existing) {
+                return res.status(400).send({ message: "Card Already exist" })
             }
             const result = await materialCollection.insertOne(material);
             res.send(result);
-         })
+        })
 
-         //! || material get by id method ||
+        //! || material get by id method ||
         //  app.get('/materials/:id', async(req, res)=>{
         //     const id = req.params.id;
         //     const query = {_id: new ObjectId(id)};
@@ -104,28 +134,28 @@ async function run() {
         //     res.send(result);
         //  })
 
-         //! || material patch id method ||
-         app.patch('/materials/:id', async(req, res)=>{
+        //! || material patch id method ||
+        app.patch('/materials/:id', async (req, res) => {
             const id = req.params.id;
             const material = req.body;
-            const filter = {sessionId: id};
+            const filter = { sessionId: id };
             const updateDoc = {
-                $set:{
+                $set: {
                     doc: material.doc,
                     image: material.image
                 }
             }
             const result = await materialCollection.updateOne(filter, updateDoc);
             res.send(result);
-         })
+        })
 
-         //! || material delete method ||
-         app.delete('/materials/:id', async(req, res)=>{
+        //! || material delete method ||
+        app.delete('/materials/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id : new ObjectId(id)};
+            const query = { _id: new ObjectId(id) };
             const result = await sessionCollection.deleteOne(query);
             res.send(result)
-         })
+        })
 
 
     } finally {
